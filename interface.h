07e31e85dcbf30b563d2e1b9775368bd0a8af3c0
liuyicheng321 <48019659+liuyicheng321@users.noreply.github.com>
2019-03-05 00:11:51 +0100

    Update interface.h

--- interface.h
+++ interface.h
@@ -39,4 +39,5 @@ extern bool init();
 extern bool create_fenetre(char* titre, SDL_Window* window);
 extern SDL_Texture* load_image(char * filename);
 extern void apply_surface(int x, int y, SDL_Texture *tex, SDL_Renderer *rend);
+extern SDL_Texture* RenderText(char* message, char* fontFile, SDL_Color color, int fontSize),
 #endif
