6dffbfdd137d45ef2e67389d340edd38f40a0851
liuyicheng321 <48019659+liuyicheng321@users.noreply.github.com>
2019-03-07 00:06:35 +0100

    Update test_interface.c

--- test_interface.c
+++ test_interface.c
@@ -1,29 +1,39 @@
 #include "interface.h"
-#include <stdio.h>
-#include <stdlib.h>
 
-int main() {
+int main() 
+{
 	SDL_Window* window = NULL;
-	if (!init())printf("erreur\n");
-	create_fenetre("tictac arena", window);
+	SDL_Renderer* renderer = NULL;
+	if (!init(&window, &renderer,"test"))exit(1);
 
-	SDL_Texture *background = NULL;
-	background = load_image("arena.bmp");
+	draw(renderer);
 
-	SDL_RenderClear(renderer);
-	int bW, bH;
-	SDL_QueryTexture(background, NULL, NULL, &bW, &bH);
-	apply_surface(0, 0, background, renderer);
-	apply_surface(bW, 0, background, renderer);
-	apply_surface(0, bH, background, renderer);
-	apply_surface(bW, bH, background, renderer);
+	SDL_Rect rect = { 0,0,10,10 };
+	Uint8 r = 0xff, g = 0xff, b = 0xff;
+	SDL_Event event;
+	while (1) {
+		SDL_PollEvent(&event);
+		SDL_RenderFillRect(renderer, &rect);
+		SDL_RenderPresent(renderer);
+		if (event.type == SDL_MOUSEMOTION)
+		{
+			rect.x = event.motion.x;
+			rect.y = event.motion.y;
+		}
+		if (event.type == SDL_MOUSEBUTTONDOWN && event.button.button == SDL_BUTTON_LEFT)
+		{
+			int row = (event.button.y - 20) / 112;
+			int column = (event.button.x - 20) / 600;
 
-	SDL_RenderPresent(renderer);
-	SDL_Delay(2000);
+			if (row >= 0 && row <= 5 && column >= 0 && column <= 1)
+			{
+				if (row >= 1 && row < 2)printf("humain vs humain");
+				if (row >= 2 && row < 3)printf("humain vs ordi");
+				if (row >= 3 && row < 4)printf("about");
+				if (row >= 4 && row < 5)break;
+			}
+		}
+	}
 
-	SDL_DestroyTexture(background);
-	SDL_DestroyRenderer(renderer);
-	SDL_DestroyWindow(window);
-
-	SDL_Quit();
+	return TRUE;
 }
