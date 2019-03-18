dd8ab2457f4f7f6575c3303014df861e067d6d5a
liuyicheng321 <48019659+liuyicheng321@users.noreply.github.com>
2019-03-18 23:44:18 +0100

    Update test_interface.c

--- test_interface.c
+++ test_interface.c
@@ -1,27 +1,56 @@
 #include "interface.h"
 
-int main() 
+int main()
 {
 	SDL_Window* window = NULL;
 	SDL_Renderer* renderer = NULL;
-	if (!init(&window, &renderer,"test"))exit(1);
-
+	if (!init(&window, &renderer, "test"))exit(1);
+	if (TTF_Init() == -1) {
+		fprintf(stderr, "Erreur d'initialisation de TTF_Init : %s\n", TTF_GetError());
+		exit(1);
+	}
+	SDL_Texture *text;
+	SDL_Rect txtDestRect;
 	draw(renderer);
 
-	SDL_Rect rect = { 0,0,10,10 };
-	Uint8 r = 0xff, g = 0xff, b = 0xff;
+	text = RenderText("TICTAC ARENA", "arial.ttf", 0xbb, 50, renderer);
+	txtDestRect.x = 135;
+	txtDestRect.y =70;
+	SDL_QueryTexture(text, NULL, NULL, &(txtDestRect.w), &(txtDestRect.h));
+	SDL_RenderCopy(renderer, text, NULL, &txtDestRect);
+
+	text = RenderText("PLAYER VS PLAYER", "arial.ttf", 0xbb, 50, renderer);
+	txtDestRect.x = 90;
+	txtDestRect.y = 182;
+	SDL_QueryTexture(text, NULL, NULL, &(txtDestRect.w), &(txtDestRect.h));
+	SDL_RenderCopy(renderer, text, NULL, &txtDestRect); 
+
+	text = RenderText("PLAYER VS COM", "arial.ttf", 0xbb, 50, renderer);
+	txtDestRect.x = 125;
+	txtDestRect.y = 294;
+	SDL_QueryTexture(text, NULL, NULL, &(txtDestRect.w), &(txtDestRect.h));
+	SDL_RenderCopy(renderer, text, NULL, &txtDestRect);
+
+	text = RenderText("ABOUT US", "arial.ttf", 0xbb, 50, renderer);
+	txtDestRect.x = 185;
+	txtDestRect.y = 406;
+	SDL_QueryTexture(text, NULL, NULL, &(txtDestRect.w), &(txtDestRect.h));
+	SDL_RenderCopy(renderer, text, NULL, &txtDestRect);
+	SDL_RenderPresent(renderer);
+
+	text = RenderText("QUIT GAME", "arial.ttf", 0xbb, 50, renderer);
+	txtDestRect.x = 180;
+	txtDestRect.y = 518;
+	SDL_QueryTexture(text, NULL, NULL, &(txtDestRect.w), &(txtDestRect.h));
+	SDL_RenderCopy(renderer, text, NULL, &txtDestRect);
+	SDL_RenderPresent(renderer);
+
 	SDL_Event event;
-	SDL_Renderer* original = renderer;
 	while (1) {
 		SDL_PollEvent(&event);
-		if (event.type == SDL_MOUSEMOTION && ((event.motion.y - 20) / 112 >= 0 )&&((event.motion.y - 20) / 112 < 1))
+		if (event.type == SDL_MOUSEBUTTONDOWN && event.button.button == SDL_BUTTON_LEFT)
 		{
-			SDL_RenderDrawLine(renderer, 15, 15, 15, 200);
-			SDL_RenderPresent(renderer);
-		}
-		else if (event.type == SDL_MOUSEBUTTONDOWN && event.button.button == SDL_BUTTON_LEFT)
-		{
-	
+
 			int row = (event.button.y - 20) / 112;
 			int column = (event.button.x - 20) / 600;
 
