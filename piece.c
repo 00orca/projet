3e4f33f06b5c98b23555c66d3b5a324504c3b68f
libanwillhem <48018918+libanwillhem@users.noreply.github.com>
2019-03-20 15:54:39 +0100

    Add files via upload

--- piece.c
+++ piece.c
@@ -1,59 +1,83 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include "piece.h"
+
 /**
 * \file piece.c
 * \brief Fonctions operants sur les pieces.
-*\author Willhem Liban
-*\version 0.5
-*\date 01 mars 2019
+*\author Willhem Liban, Eliot Lepoittevin
+*\version 3.0
+*\date 20 mars 2019
 */
 
 
-piece_t * init_piece(classe_t classe){
+piece_t * init_piece(classe_t classe,int id_joueur){
+    int pos=((rand()%(4)) + 1);
     piece_t * piece=malloc(sizeof(piece_t));
     switch (classe){
-        case 0:
-            piece->nom_classe=malloc(sizeof(char)*strlen("knight")+1);
-            piece->nom_classe = "knight";
+        case knight:
+            piece->classe = knight;
             piece->pdv=50;
-            piece->puissance = 22;
-            piece->armure=25;
-            piece->block=80;
+            piece->puissance = 12;
+            piece->armure=15;
+            piece->block=60;
             piece->portee=1;
-            piece->deplacement=3;
+            piece->deplacement=2;
+            piece->select=0;
+            piece->joueur=id_joueur;
+            piece->direction=pos;
             break;
-        case 1:
-            piece->nom_classe=malloc(sizeof(char)*strlen("scout")+1);
-            piece->nom_classe = "scout";
-            piece->pdv=40;
+        case scout:
+            piece->classe = scout;
+            piece->pdv=30;
             piece->puissance = 18;
             piece->armure=8;
-            piece->block=60;
+            piece->block=30;
             piece->portee=6;
             piece->deplacement=4;
+            piece->select=0;
+            piece->joueur=id_joueur;
+            piece->direction=pos;
             break;
-        case 2:
-            piece->nom_classe=malloc(sizeof(char)*strlen("priest")+1);
-            piece->nom_classe = "priest";
+        case priest:
+            piece->classe = priest;
             piece->pdv=24;
             piece->puissance = 12;
-            piece->armure=0;
-            piece->block=0;
+            piece->armure=2;
+            piece->block=10;
             piece->portee=3;
             piece->deplacement=3;
+            piece->select=0;
+            piece->joueur=id_joueur;
+            piece->direction=pos;
             break;
-        case 3:
-            piece->nom_classe=malloc(sizeof(char)*strlen("magician")+1);
-            piece->nom_classe = "magician";
+        case magician:
+            piece->classe = magician;
             piece->pdv=28;
             piece->puissance = 24;
-            piece->armure=0;
+            piece->armure=3;
             piece->block=20;
             piece->portee=4;
             piece->deplacement=4;
-        default: printf("erreur dans la création d'une piece\n");
+            piece->select=0;
+            piece->joueur=id_joueur;
+            piece->direction=pos;
+            break;
+        case faucheuse:
+            piece->classe = faucheuse;
+            piece->pdv=100;
+            piece->puissance = 100;
+            piece->armure=15;
+            piece->block=40;
+            piece->portee=4;
+            piece->deplacement=10;
+            piece->select=0;
+            piece->joueur=id_joueur;
+            piece->direction=pos;
+            break;
+        default: printf("Cette classe n'existe pas.\n");
     }
     return (piece);
 }
@@ -62,41 +86,284 @@ int piece_existe(piece_t * piece){
     return(!(piece==NULL));
 }
 
-void afficher_piece(piece_t * piece){
-    if (piece_existe(piece)){
-        printf("classe : %s\n", piece->nom_classe);
-        printf("lp : %d\n", piece->pdv);
-        printf("pui : %d\n", piece->puissance);
-        printf("armure : %d\n", piece->armure);
-        printf("block : %d\n", piece->block);
-        printf("portee : %d\n",piece->portee);
-        printf("deplacement : %d\n", piece->deplacement);
-    }
-    else
-        printf("Pas de piece a afficher.\n");
-}
 
-int destruction_piece(piece_t ** piece){
-    if(piece_existe(*piece)){
-        //free((*piece)->nom_classe);
-        free(*piece);
-        (*piece)=NULL;
+int destruction_piece(piece_t * piece){
+    if(piece_existe(piece)){
+        free(piece);
+        (piece)=NULL;
         return 1;
     }
     return 0;
 }
 
+void pathfinding(case_t terrain[N][M], int x, int y){
+  int deplacement[N][M];
+  int i,j;
+  int cpt;
+    //initialisation des deplacements possibles
+    for (i=0;i<N;i++){//parcours du terrain
+      for(j=0;j<M;j++){
+        terrain[i][j].deplacement=0;
+      }
+    }
 
-int main(int argc, char const *argv[]) {
-    piece_t * knight1 = NULL;
-    printf("création.............\n");
-    knight1=init_piece(0);
-    printf("affichage............\n");
-    afficher_piece(knight1);
-    printf("destruction..........\n");
-    destruction_piece(&knight1);
-    printf("affichage............\n");
-    afficher_piece(knight1);
-    printf("fin\n");
-    return 0;
+
+    for (i=0;i<N;i++){//parcours du terrain
+        for(j=0;j<M;j++){
+            if (terrain[i][j].type==5 || terrain[i][j].piece!=NULL){
+                deplacement[i][j]=-1;
+            }//case d'eau deplacement impossible
+            else{
+              deplacement[i][j]=0;
+            }
+        }
+
+
+    }
+    deplacement[x][y]=1;//case ou se trouve la piece
+
+
+    for(cpt=1;cpt<=terrain[x][y].piece->deplacement;cpt++){//tant qu'il y a des point de deplacement
+        for (i=0;i<N;i++){//parcours du terrain
+            for(j=0;j<M;j++){
+                if (deplacement[i][j]==cpt){
+                    if(i+1<N && deplacement[i+1][j]==0)
+                        deplacement[i+1][j]=cpt+1;
+                    if(i-1>=0 && deplacement[i-1][j]==0)
+                        deplacement[i-1][j]=cpt+1;
+                    if(j+1<M && deplacement[i][j+1]==0)
+                        deplacement[i][j+1]=cpt+1;
+                    if(j-1>=0 && deplacement[i][j-1]==0)
+                        deplacement[i][j-1]=cpt+1;
+
+                }
+
+            }
+        }
+    }
+
+    for (i=0;i<N;i++){//parcours du terrain
+        for(j=0;j<M;j++){
+            if(deplacement[i][j]>1){
+              terrain[i][j].deplacement=1;
+            }
+        }
+    }
+
+}
+
+
+
+
+void move(case_t terrain[N][M],int x,int y,int joueur,joueurs_t tab[J]){
+  if(terrain[x][y].deplacement==1){
+    for(int compteur=0;compteur<N;compteur++){
+      for(int compteur2=0;compteur2<M; compteur2++){
+        if(terrain[compteur][compteur2].piece && terrain[compteur][compteur2].piece->select==1 && terrain[compteur][compteur2].piece->joueur==joueur){
+
+
+          terrain[x][y].piece=malloc(sizeof(piece_t));
+          fprintf(stderr,"compteur compteur2 = %d\n",terrain[compteur][compteur2].piece->classe);
+          terrain[x][y].piece->classe = terrain[compteur][compteur2].piece->classe;
+          terrain[x][y].piece->pdv = terrain[compteur][compteur2].piece->pdv;
+          terrain[x][y].piece->puissance = terrain[compteur][compteur2].piece->puissance;
+          terrain[x][y].piece->armure = terrain[compteur][compteur2].piece->armure;
+          terrain[x][y].piece->block = terrain[compteur][compteur2].piece->block;
+          terrain[x][y].piece->portee = terrain[compteur][compteur2].piece->portee;
+          terrain[x][y].piece->deplacement = terrain[compteur][compteur2].piece->deplacement;
+          terrain[x][y].piece->select = terrain[compteur][compteur2].piece->select;
+          terrain[x][y].piece->joueur = terrain[compteur][compteur2].piece->joueur;
+          terrain[x][y].piece->direction = terrain[compteur][compteur2].piece->direction;
+
+
+          //destruction_piece(terrain[compteur][compteur2].piece);
+          free(terrain[compteur][compteur2].piece);
+          terrain[compteur][compteur2].piece=NULL;
+
+          terrain[x][y].piece->select=1;
+          //pathfinding(terrain,x,y);
+
+          tab[joueur].pts_action_actu--;
+
+          return;
+        }
+      }
+    }
+
+  }
+}
+void pathfinding_combat(case_t terrain[N][M], int x, int y,int joueur_actu){
+  int attaque[N][M];
+  int i,j;
+  int cpt;
+  //initialisation des attaques possibles
+  for (i=0;i<N;i++){//parcours du terrain
+      for(j=0;j<M;j++){
+        terrain[i][j].attaque=0;
+      }
+  }
+
+  for (i=0;i<N;i++){//parcours du terrain
+      for(j=0;j<M;j++){
+        attaque[i][j]=0;
+      }
+  }
+
+    attaque[x][y]=1;//case ou se trouve la piece
+
+    for(cpt=1;cpt<=terrain[x][y].piece->portee;cpt++){//tant qu'il y a des point de la portée
+        for (i=0;i<N;i++){//parcours du terrain
+            for(j=0;j<M;j++){
+                if (attaque[i][j]==cpt){
+                  if(i+1<N && attaque[i+1][j]==0)
+                    attaque[i+1][j]=cpt+1;//ICI
+                  if(i-1>=0 && attaque[i-1][j]==0)
+                    attaque[i-1][j]=cpt+1;
+                  if(j+1<N && attaque[i][j+1]==0)
+                    attaque[i][j+1]=cpt+1;
+                  if(j-1>=0 && attaque[i][j-1]==0)
+                    attaque[i][j-1]=cpt+1;
+                }
+
+            }
+        }
+    }
+    for (i=0;i<N;i++){
+      for(j=0;j<M;j++){
+        if(terrain[i][j].piece && terrain[i][j].piece->joueur==joueur_actu && attaque[i][j]!=0)
+          attaque[i][j]=-2;
+      }
+    }
+
+    for (i=0;i<N;i++){//parcours du terrain
+        for(j=0;j<M;j++){
+            if(attaque[i][j]>1 && terrain[i][j].piece && terrain[i][j].piece->joueur!=joueur_actu && terrain[x][y].piece->classe!=priest)
+              terrain[i][j].attaque=1;
+            if(attaque[i][j]==-2 && terrain[i][j].piece && terrain[i][j].piece->joueur==joueur_actu && terrain[x][y].piece->classe==priest)
+              terrain[i][j].attaque=1;
+        }
+    }
+}
+
+int calc_block(case_t terrain [N][M],int x_att, int y_att, int x_def,int y_def){
+
+    int pos_block[N][M];
+    int i,j;
+    int cpt=1;
+    //init matrice a 0
+    for(i=0;i<N;i++){
+        for(j=0;j<M;j++)
+            pos_block[i][j]=-2;
+    }
+    pos_block[x_def][y_def]=0;
+    //diagonales a -1
+    while (x_def-cpt>=0 && y_def+cpt<M){
+        pos_block[x_def-cpt][y_def+cpt]=-1;
+        cpt++;
+    }
+    cpt=1;
+    while (x_def+cpt-1<N && y_def+cpt<N){
+        pos_block[x_def+cpt][y_def+cpt]=-1;
+        cpt++;
+    }
+    while (x_def+cpt<N && y_def-cpt>=0){
+        pos_block[x_def+cpt][y_def-cpt]=-1;
+        cpt++;
+    }
+    cpt=1;
+    while (x_def-cpt>=0 && y_def-cpt>=0){
+        pos_block[x_def-cpt][y_def-cpt]=-1;
+        cpt++;
+    }
+    for(i=0;i<N;i++){
+        for(j=0;j<M;j++){
+            if ((pos_block[i][j-1]==-1 || pos_block[i][j-1]==0 || pos_block[i][j-1]==1) && j>y_def)
+                pos_block[i][j]=1;
+        }
+    }
+    for(i=0;i<N;i++){
+        for(j=M-1;j>=0;j--){
+            if ((pos_block[i][j+1]==-1 || pos_block[i][j+1]==0 || pos_block[i][j-1]==2) && j<y_def)
+            pos_block[i][j]=2;
+        }
+    }
+    for(j=0;j<M;j++){
+        for(i=0;i<N;i++){
+            if ((pos_block[i-1][j]==-1 || pos_block[i-1][j]==0 || pos_block[i-1][j]==3) && i>x_def)
+                pos_block[i][j]=3;
+        }
+    }
+    for(j=0;j<M;j++){
+        for(i=0;i<N;i++){
+            if (pos_block[i][j]==-2)
+                pos_block[i][j]=4;
+        }
+    }
+    if(terrain[x_def][y_def].piece->direction==haut){
+        if (pos_block[x_att][y_att]==2)
+            return 0;
+        else if (pos_block[x_att][y_att]==3||pos_block[x_att][y_att]==4||pos_block[x_att][y_att]==-1)
+            return (terrain[x_def][y_def].piece->block/2);
+        else
+            return (terrain[x_def][y_def].piece->block);
+    }
+    else if(terrain[x_def][y_def].piece->direction==bas){
+        if (pos_block[x_att][y_att]==1)
+            return 0;
+        else if (pos_block[x_att][y_att]==3||pos_block[x_att][y_att]==4||pos_block[x_att][y_att]==-1)
+            return (terrain[x_def][y_def].piece->block/2);
+        else
+            return (terrain[x_def][y_def].piece->block);
+    }
+    else if(terrain[x_def][y_def].piece->direction==gauche){
+        if (pos_block[x_att][y_att]==4)
+            return 0;
+        else if (pos_block[x_att][y_att]==1||pos_block[x_att][y_att]==2||pos_block[x_att][y_att]==-1)
+            return (terrain[x_def][y_def].piece->block/2);
+        else
+            return (terrain[x_def][y_def].piece->block);
+    }
+    else if(terrain[x_def][y_def].piece->direction==droite){
+        if (pos_block[x_att][y_att]==3)
+            return 0;
+        else if (pos_block[x_att][y_att]==1||pos_block[x_att][y_att]==2||pos_block[x_att][y_att]==-1)
+            return (terrain[x_def][y_def].piece->block/2);
+        else
+            return (terrain[x_def][y_def].piece->block);
+    }
+    return -1;
+}
+
+void combat(case_t terrain [N][M],int x_att, int y_att, int x_def,int y_def,int joueur,joueurs_t tab[J]){
+    if(terrain[x_att][y_att].piece->classe==priest){//soin si la piece selectionné est un pretre
+        soin(terrain,x_att,y_att,x_def,y_def,joueur,tab);
+        return;
+    }
+    srand(time(NULL));
+    if(terrain[x_def][y_def].attaque==1){//si la piece attaquée est sur une case a portée !!!DOUTE!!!
+      int blockd=(rand()%(100) + 1);//att bloquée ou non
+      int blockage=calc_block(terrain,x_att,y_att,x_def,y_def);
+      fprintf(stderr,"blockage : %d\n",blockage);
+      if(blockd>blockage){
+        terrain[x_def][y_def].piece->pdv-=terrain[x_att][y_att].piece->puissance-(terrain[x_def][y_def].piece->armure);//armure=absorption de dégats
+        if(terrain[x_def][y_def].piece->pdv<=0){   //mort ou non de la piece ennemi
+          tab[terrain[x_def][y_def].piece->joueur].nb_unite--;
+          free(terrain[x_def][y_def].piece);
+          terrain[x_def][y_def].piece=NULL;
+        }
+      }
+      fprintf(stderr,"attaque reussie\n");
+      tab[joueur].pts_action_actu--;
+    }
+    else
+        printf("echec de l'attaque (hors de portée).\n");
+}
+
+void soin(case_t terrain [N][M],int x_att, int y_att, int x_def,int y_def,int joueur,joueurs_t tab[J]){
+    if(terrain[x_def][y_def].attaque==1){
+        terrain[x_def][y_def].piece->pdv+=terrain[x_att][y_att].piece->puissance;
+        tab[joueur].pts_action_actu--;
+      }
+    else
+        printf("echec du soin (hors de portée).\n");
 }
--- piece.h
+++ piece.h
@@ -1,29 +1,122 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_image.h>
+#define N 20 //taille de la grille
+#define M 20
+#define J 5 //nb de joueur
+#define PRESET 1 //1 pour generation alea, autre pour preset de carte via fichier
+#define NB_CLASSE 5
 
 /**
 * \file piece.c
-* \brief Structure pieces.
-*\author Willhem Liban
-*\version 0.5
-*\date 01 mars 2019
+* \brief Fonctions operants sur les pieces.
+*\author Willhem Liban, Eliot Lepoittevin
+*\version 3.0
+*\date 20 mars 2019
 */
 
+
+/**
+*\struct piece_s
+*\brief Objet piece qui contient les statistiques d'une piece (unité qui combat)
+*contient : les points de vie, la classe, la uissance, armure,block, portee, si oui ou non la piece est selectionnée, la direction dans laquelle est la piece et le joueur a qui la piece appartient
+*/
+/**
+*\struct case_s
+*\brief Objet qui represente une case, la piece qui est sur cette case, sa position et l'image qui lui corresond
+*/
+/**
+*\struct joueurs_s
+*\brief Objet qui represente un joueur avec ses points d'actions, le nombre d'unités qu'il possede.
+*/
 typedef enum {
-    knight, scout,priest,magician
+    knight=1,scout,priest,magician,faucheuse
 }classe_t;
+
+typedef enum{
+    haut=1,bas,gauche,droite
+}dir_t;
+
 typedef struct piece_s{
-    char * nom_classe;
+    classe_t classe;
     int pdv; //point de vie
     int puissance;
     int armure;
     int block; //pourcentage de chance de bloquer
     int portee; //portée de l'attaque
     int deplacement; //nombre de case de mouvement
+    int select;
+    int joueur;
+    dir_t direction;
 }piece_t;
 
 
-piece_t * init_piece(classe_t classe);
+
+
+typedef struct case_s{
+	int type;				//type d'une case
+	int climat;
+	piece_t *piece;		//piece sur cette case
+	int deplacement;  // 1 -> droit de se deplacer , 0pas le droit de se deplacer
+	int attaque;    // 1 -> droit d'attaquer , 0 -> pas le droit d'attaquer
+	int xImg; // ccordonné reel de l'image
+	int yImg;
+	int x1; // coordonné du losange représentant une case(pour gerer les coordonnées du clic souris)
+	int y1;
+	int x2;
+	int y2;
+  int x3;
+	int y3;
+	int x4;
+	int y4;
+}case_t;
+
+
+typedef struct joueurs_s{
+  int pts_action_max;
+  int pts_action_actu;
+  int nb_unite;
+  int id_joueur;
+
+}joueurs_t;
+
+/**
+*\fn piece_t * init_piece(classe_t classe,int id_joueur);
+*\brief Fonction pour creer les pieces
+*
+*\fn int piece_existe(piece_t * piece)
+*\brief Renvoi 1 si la piece passée en parametre existe
+*
+*\fn int destruction_piece(piece_t * piece)
+*\brief Detruit une piece
+*
+*\fn void pathfinding(case_t terrain[N][M], int x, int y)
+*\brief sert a connaitre les cases ou peut se deplacer une unité
+*
+*\fn void pathfinding_combat(case_t terrain[N][M], int x, int y,int joueur_actu)
+*\brief sert a connaitre les cases ou peut attaquer une unité
+*
+*\fn int calc_block(case_t terrain [N][M],int x_att, int y_att, int x_def,int y_def)
+*\brief calcul le block d'une unité en fonction d'ou elle est attaquée
+*\fn void combat(case_t terrain [N][M],int x_att, int y_att, int x_def,int y_def,int joueur,joueurs_t tab[J])
+*\brief calcul les dommages en cas de combat
+*\fn void soin(case_t terrain [N][M],int x_att, int y_att, int x_def,int y_def,int joueur,joueurs_t tab[J])
+*\brief ajoute les points de vie en cas de soin
+*\fn void move(case_t terrain[N][M],int x,int y, int joueur,joueurs_t tab[J])
+*\brief permet de deplacer les unités sur le terrain
+*/
+piece_t * init_piece(classe_t classe,int id_joueur);
 int piece_existe(piece_t * piece);
-int destruction_piece(piece_t ** piece);
+/*Detruit une piece*/
+int destruction_piece(piece_t * piece);
+/*Affiche les chemins possible pour la piece selectionnée dont les coordonnées sont passée en param*/
+void pathfinding(case_t terrain[N][M], int x, int y);
+void pathfinding_combat(case_t terrain[N][M], int x, int y,int joueur_actu);
+/*effectue le calcul des dommages*/
+int calc_block(case_t terrain [N][M],int x_att, int y_att, int x_def,int y_def);
+void combat(case_t terrain [N][M],int x_att, int y_att, int x_def,int y_def,int joueur,joueurs_t tab[J]);
+void soin(case_t terrain [N][M],int x_att, int y_att, int x_def,int y_def,int joueur,joueurs_t tab[J]);
+void move(case_t terrain[N][M],int x,int y, int joueur,joueurs_t tab[J]);
